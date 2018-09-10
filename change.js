2409c576f141b259a210ffac1bb66270cc236e46
Suguru Oki <oki.suguru@hamee.co.jp>
2018-09-10 11:11:41 +0900

    [追加] タイムライン部分の背景色変更を追加

--- change.js
+++ change.js
@@ -1,4 +1,8 @@
-$('#globalHeader').css('background-color', '#28b9ff');
-$('.p-home_menuItem-active').css('background-color', '#28b9ff');
+const themeColor = '#28b9ff';
+
+$('#globalHeader').css('background-color', themeColor);
+$('.p-home_menuItem-active').css('background-color', themeColor);
 $('.st-Header_postButton').css('background-color', 'blue');
+console.dir($('.tl-ItemList'));
+$('.tl-ItemList').css('background-color', themeColor);
 $('.p-home_ad').remove();

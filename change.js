805a9013380ad7a5f9028b9f9b051efebf13b023
Suguru Oki <oki.suguru@hamee.co.jp>
2018-09-10 11:40:53 +0900

    [修正] 強調色について定数として定義

--- change.js
+++ change.js
@@ -1,8 +1,7 @@
 const themeColor = '#28b9ff';
+const emphasisColor = 'blue';
 
 $('#globalHeader').css('background-color', themeColor);
 $('.p-home_menuItem-active').css('background-color', themeColor);
-$('.st-Header_postButton').css('background-color', 'blue');
-console.dir($('.tl-ItemList'));
-$('.tl-ItemList').css('background-color', themeColor);
+$('.st-Header_postButton').css('background-color', emphasisColor);
 $('.p-home_ad').remove();

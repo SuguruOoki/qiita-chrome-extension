59c7b2d2de6001c05eec868de18e43660e41b5d6
Suguru Oki <oki.suguru@hamee.co.jp>
2018-09-10 11:41:24 +0900

    [追加] パスによって色付けするclassが違うため、pathの判定を追加

--- change.js
+++ change.js
@@ -5,3 +5,21 @@ $('#globalHeader').css('background-color', themeColor);
 $('.p-home_menuItem-active').css('background-color', themeColor);
 $('.st-Header_postButton').css('background-color', emphasisColor);
 $('.p-home_ad').remove();
+
+let path = location.pathname;
+
+if (path === '/') {
+	$('.tr-ItemList').css('background-color', themeColor);
+}
+
+if (path === '/timeline') {
+	$('.tl-ItemList').css('background-color', themeColor);
+	$('.tl-ItemList_moreButton').css('background-color', emphasisColor);
+}
+
+if (path === '/tag-feed') {
+	$('.tf-ItemList').css('background-color', themeColor);
+	$('.tf-ItemList_moreButton').css('background-color', emphasisColor);
+}
+
+

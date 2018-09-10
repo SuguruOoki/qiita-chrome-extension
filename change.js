0476cef42488927308a96a6545ddf67de7b3b189
Suguru Oki <oki.suguru@hamee.co.jp>
2018-09-10 10:37:57 +0900

    [修正] cssの箇所を修正し、右上の広告を削除するjQueryを追加

--- change.js
+++ change.js
@@ -1,3 +1,4 @@
 $('#globalHeader').css('background-color', '#28b9ff');
 $('.p-home_menuItem-active').css('background-color', '#28b9ff');
 $('.st-Header_postButton').css('background-color', 'blue');
+$('.p-home_ad').remove();
